3c487636b0519bc65a51163a8229d2a49ca09d85
Nathan Li <moncskoli@gmail.com>
2021-09-10 00:38:46 -0400

    feature 2 part 1

--- components/CaseComponents/Modals/AddCaseModal.tsx
+++ components/CaseComponents/Modals/AddCaseModal.tsx
@@ -45,9 +45,14 @@ type AddCaseModalProps = {
   in this variable 
 */
 const InsertCaseMutation = `
-query MyQuery {
-  __typename # Placeholder value
-}
+mutation AddCaseMutation($description: String = "", $name: String = "", $status: String = "", $category_id: Number = 10) {
+    insert_cases_one(object: {description: $description, name: $name, status: $status, category_id: $category_id}) {
+      description
+      name
+      status
+      category_id
+    }
+  }  
 `;
 // END TODO
 
@@ -129,6 +134,11 @@ const AddCaseModal: React.FC<AddCaseModalProps> = (props) => {
                 to render a MenuItem with category id as the value, and the 
                 category name as the text.
               */}
+              {
+                <MenuItem key={index} value={category.id}>
+                  {category.name}
+                </MenuItem>
+              }
               {/* END TODO */}
             </Select>
           </FormControl>
